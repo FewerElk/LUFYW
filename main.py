3a341461a6be7b86ff7deb751e8d92650f9764aa
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-07-20 13:18:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,37 @@ class LUFYW(object):
     def __init__(self):
         ...
         
-    def calculate(self, points:tuple, ways:tuple, type:str):
-        ...
+    def calculate(self, points:tuple, ways:tuple, type:str, p1:Point, p2:Point):
+        self.browsed = []
+        if type == "--shortest":
+            try:
+                return self._rec(p1, p2)
+            except:
+                return (False,)
+            
+    def _rec(self, base, stop):
+        for pt in base.connectedto:
+            if pt in self.browsed:
+                continue
+            if stop.ID == pt.ID:
+                return [True, base, stop]
+            else:
+                rep = self._rec(pt, stop)
+                if rep[0]:
+                    return rep.append(base)
+                
+                
         
 class Point(object):
-    def __init__(self, id:int, ways:tuple):
+    def __init__(self, id:int, conto:tuple):
         self.ID = id
-        self.WAYS = ways
+        self.connectedto = conto
         ...
         
 class Way(object):
-    def __init__(self, id:int, p1:Point, p2:Point):
+    def __init__(self, id:int, p1:Point, p2:Point, dist:int):
         self.ID = id
         self.P1 = p1
         self.P2 = p2
+        self.distance = dist
         
