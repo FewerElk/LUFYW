9bb85870b08ceb78a25538395c1521259faac7ba
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-07-20 12:33:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ class LUFYW(object):
     def __init__(self):
         ...
         
-class Way(object):
-    def __init__(self):
+    def calculate(self, points:tuple, ways:tuple, type:str):
         ...
+        
+class Point(object):
+    def __init__(self, id:int, ways:tuple):
+        self.ID = id
+        self.WAYS = ways
+        ...
+        
+class Way(object):
+    def __init__(self, id:int, p1:Point, p2:Point):
+        self.ID = id
+        self.P1 = p1
+        self.P2 = p2
+        
